1898d5f3122fd0fa5d8ec9def1482b3d6091da11
Dana-Codes <codingbydaniel@gmail.com>
2023-06-20 18:41:24 -0700

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
 
-/*
- * File: 2-print_alphabet_x10.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+    int i;
+    char letter;
 
-	while (count++ <= 9)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+
+        _putchar('\n');
+    }
 }
