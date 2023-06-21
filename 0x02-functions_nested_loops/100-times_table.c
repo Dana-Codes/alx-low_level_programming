ef38510da2564d76614a9e599ef87e5c68d5645a
Dana-Codes <codingbydaniel@gmail.com>
2023-06-21 15:13:25 -0700

    correted

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,49 +8,36 @@
  */
 void print_times_table(int n)
 {
-    int i, j;
+    int i, j, product;
 
-    if (n > 0 && n < 15)
+    if (n >= 0 && n <= 15)
     {
         for (i = 0; i <= n; i++)
         {
-            _putchar('0');
-            for (j = 1; j <= n; j++)
-                putformat(i * j);
+            for (j = 0; j <= n; j++)
+            {
+                product = i * j;
+                if (j != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    if (product < 100)
+                        _putchar(' ');
+                    if (product < 10)
+                        _putchar(' ');
+                }
+                if (product >= 100)
+                {
+                    _putchar((product / 100) + '0');
+                    _putchar(((product / 10) % 10) + '0');
+                }
+                else if (product >= 10)
+                {
+                    _putchar((product / 10) + '0');
+                }
+                _putchar((product % 10) + '0');
+            }
             _putchar('\n');
         }
-    }
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-    if (n <= 9)
-    {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(n + '0');
-    }
-    else if (n > 9 && n <= 99)
-    {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(n / 10 + '0');
-        _putchar(n % 10 + '0');
-    }
-    else
-    {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(n / 100 + '0');
-        _putchar(n / 10 % 10 + '0');
-        _putchar(n % 10 + '0');
     }
 }
