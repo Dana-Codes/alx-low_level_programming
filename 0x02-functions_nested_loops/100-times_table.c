6bfea83a08ac1fc31ff6f79dc5b3ed362d0bff08
Dana-Codes <codingbydaniel@gmail.com>
2023-06-21 21:07:43 -0700

    betty corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,48 @@
 
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - prints times table for numbers from 0-14
+ * print_times_table - prints the times table for numbers from 0 to 15
  * @n: An input integer value
  * Return: Nothing
  */
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, product;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                product = i * j;
-                if (j != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    if (product < 100)
-                        _putchar(' ');
-                    if (product < 10)
-                        _putchar(' ');
-                }
-                if (product >= 100)
-                {
-                    _putchar((product / 100) + '0');
-                    _putchar(((product / 10) % 10) + '0');
-                }
-                else if (product >= 10)
-                {
-                    _putchar((product / 10) + '0');
-                }
-                _putchar((product % 10) + '0');
-            }
-            _putchar('\n');
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+
+				if (j != 0)
+				{
+					putchar(',');
+					putchar(' ');
+
+					if (product < 100)
+						putchar(' ');
+					if (product < 10)
+						putchar(' ');
+				}
+
+				if (product >= 100)
+				{
+					putchar((product / 100) + '0');
+					putchar(((product / 10) % 10) + '0');
+				}
+				else if (product >= 10)
+				{
+					putchar((product / 10) + '0');
+				}
+
+				putchar((product % 10) + '0');
+			}
+
+			putchar('\n');
+		}
+	}
 }
