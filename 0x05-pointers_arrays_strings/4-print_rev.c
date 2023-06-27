26bdc12203421eadedfd8d5abc8eb0d5ed940430
Dana-Codes <codingbydaniel@gmail.com>
2023-06-27 08:16:42 -0700

    Write a function that reverses a string.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,26 +2,18 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void print_rev(char *s)
 {
-	int i, j, len;
+	int len = 0, index;
 
-	i = 0;
+	while (s[index++])
+		len++;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	_putchar('\n');
 }
