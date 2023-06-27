296a0bf962877faff924eec78e33a8e285e73fef
Dana-Codes <codingbydaniel@gmail.com>
2023-06-27 09:35:44 -0700

    corrected

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(0));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        /* Generate random uppercase letter or digit */
+        if (rand() % 2)
+            password[i] = 'A' + rand() % 26;
+        else
+            password[i] = '0' + rand() % 10;
+    }
+
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("%s\n", password);
+
+    return 0;
+}
