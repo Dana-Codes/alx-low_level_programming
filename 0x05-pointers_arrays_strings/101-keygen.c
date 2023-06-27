adb33fb47b12056243049543547373f071cde883
Dana-Codes <codingbydaniel@gmail.com>
2023-06-27 11:31:04 -0700

    corrected

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,23 +6,19 @@
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    int i;
+	char password[PASSWORD_LENGTH + 1];
+	int i;
 
-    srand(time(0));
-
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        /* Generate random lowercase letter or digit */
-        if (rand() % 2)
-            password[i] = 'a' + rand() % 26;
-        else
-            password[i] = '0' + rand() % 10;
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("%s", password);
-
-    return 0;
+	srand(time(0));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		/* Generate random lowercase letter or digit */
+		if (rand() % 2)
+			password[i] = 'a' + rand() % 26;
+		else
+			password[i] = '0' + rand() % 10;
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s", password);
+	return (0);
 }
